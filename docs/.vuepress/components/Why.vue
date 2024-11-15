87373665bae03e1ae1e198ce5468e65a2b432117
ZXLee <admin@zxlee.cn>
2024-11-15 23:46:32 +0800

    Update Why.vue

--- docs/.vuepress/components/Why.vue
+++ docs/.vuepress/components/Why.vue
@@ -5,7 +5,7 @@
                 <div class="box">
                     <div class="icon">{{ item.icon }}</div>
                     <div class="title">{{ item.title }}</div>
-                    <div class="details">{{ item.details }}</div>
+                    <div class="details" v-html="item.details"></div>
                 </div>
             </div>
         </div>
@@ -19,27 +19,27 @@
 				list: [{
                     icon: '💧',
                     title: '像喝水一样简单！',
-                    details: '仅需两步轻松完成完整下拉刷新和上拉加载更多功能，分页全自动管理！亦可通过极少代码轻松完成标准页面容器布局。'
+                    details: '<span style="font-weight: bold;color: #007aff;">仅需两步</span>即可实现完整下拉刷新和上拉加载更多功能，分页全自动管理！亦可通过<span style="font-weight: bold;color: #007aff;">极少代码</span>轻松完成标准页面容器布局。'
                 },{
                     icon: '⚒️',
                     title: '丰富的自定义配置！',
-                    details: '支持自定义下拉刷新、上拉加载更多，支持自动管理空数据图，支持下拉进入二楼，支持聊天分页、本地分页，支持国际化等数百项个性化配置。'
+                    details: '支持自定义下拉刷新、上拉加载更多，支持自动管理空数据图，支持下拉进入二楼，支持聊天分页、本地分页，支持国际化等<span style="font-weight: bold;color: #007aff;">数百项个性化配置</span>。'
                 },{
                     icon: '🚀',
                     title: '高性能！',
-                    details: '使用wxs+renderjs在视图层实现下拉刷新，丝滑流畅零卡顿；强大的兼容各平台的虚拟列表算法，轻松渲染百万级列表数据。'
+                    details: '使用wxs+renderjs在视图层实现下拉刷新，<span style="font-weight: bold;color: #007aff;">丝滑流畅零卡顿</span>；强大的兼容各平台的虚拟列表算法，轻松渲染<span style="font-weight: bold;color: #007aff;">百万级列表数据</span>。'
                 },{
                     icon: '🌍',
                     title: '全平台兼容！',
-                    details: '支持ios、安卓、鸿蒙next；h5；微信小程序、QQ小程序、支付宝小程序、抖音小程序、百度小程序、飞书小程序、快应用等平台。'
+                    details: '支持ios、安卓、鸿蒙next；h5；微信小程序、QQ小程序、支付宝小程序、抖音小程序、百度小程序、快手小程序、飞书小程序、快应用等<span style="font-weight: bold;color: #007aff;">十几个平台</span>。'
                 },{
                     icon: '🔥',
                     title: '热门且流行！',
-                    details: '在插件市场+npm中已被开发者下载超10万次，质量、扩展性和稳定性经过数万个项目检验。在uniapp插件市场热门总榜中排名前十。'
+                    details: '在插件市场+npm中已被开发者下载<span style="font-weight: bold;color: #007aff;">超10万次</span>，质量、扩展性和稳定性经过<span style="font-weight: bold;color: #007aff;">数万个项目</span>检验。在uniapp插件市场热门总榜中稳定<span style="font-weight: bold;color: #007aff;">排名前十</span>。'
                 },{
                     icon: '🌟',
                     title: '广受好评且可靠！',
-                    details: '由2021年开源至今已迭代近200个版本，在插件市场评分为4.8/5分。建有总计容纳7000人的QQ群，使用问题、bug及需求解答及时。'
+                    details: '由2021年开源至今已迭代<span style="font-weight: bold;color: #007aff;">100+</span>个版本，在插件市场评分<span style="font-weight: bold;color: #007aff;">4.8</span>/5.0分。建有总计容纳7000人的QQ群，使用问题、bug及需求解答及时。'
                 }]
 			}
 		}
